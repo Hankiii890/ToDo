549808e88261f1364c456a503d159d373f44b59a
Hankiii890 <shenderuk4kir@gmail.com>
2024-03-17 19:04:41 +0300

    fix

--- MyApp/App/views.py
+++ MyApp/App/views.py
@@ -7,6 +7,8 @@ class TodoViewsSet(
     viewsets.ModelViewSet):  # Класс предоставляет стандартные методы для представления модели Todo в виде веб-API.
     queryset = Todo.objects.all
     serializer_class = TodoSerializer
-    permission_classes = [permissions.AllowAny]  # Доступ к методам представления разрешён всем пользователям
+    permission_classes = [
+        permissions.AllowAny
+    ]  # Доступ к методам представления разрешён всем пользователям
 
 
